92efabcd04efab90ec7fe0ca8fb2ef416278311e
WARP4dot52 <warp@tutanota.de>
2025-08-04 21:59:05 +0200

    add stdin

--- meow.c
+++ meow.c
@@ -4,13 +4,23 @@ int main(int argc, char* argv[]){
     int i;
     int c;
     FILE* fp;
-    for(i=1; i<argc; i++){
-        fp = fopen(argv[i], "r");
-        if(fp == NULL){ printf("\"%s\" not found \n", argv[i]); continue; }
-        while((c = fgetc(fp)) != EOF){
-            printf("%c", c);
-        }
-        fclose(fp);
+    /* if file is passed concatenate */
+    if(argc > 2){
+        for(i=1; i<argc; i++){
+          fp = fopen(argv[i], "r");
+          if(fp == NULL){ printf("\"%s\" not found \n", argv[i]); continue; }
+          while((c = fgetc(fp)) != EOF){
+              printf("%c", c);
+          }
+          fclose(fp);
+      }
     }
-    return 0;
+    /* else read from stdin */
+    else{
+      while((c = fgetc(stdin)) != EOF){
+          printf("%c", c);
+      }
+    }
+
+   return 0;
 }
